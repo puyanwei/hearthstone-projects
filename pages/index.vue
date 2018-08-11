59dd61541d6ef1f89382aa4dcaaf46bf55269960
Puyan Wei <puyanwei@gmail.com>
2018-08-11 13:53:29 +0100

    Create basic intro page, setup test page that routes correctly

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,19 @@
 <template>
     <div class="container">
         <h1>Layout Training</h1>
-       <p>This is my layout training website that I'm going to use to practice copying webpages. 
-    I will be trying to use CSS grid most of the time, using the minimal amount of code possible to achieve the correct responsive designs.
-       </p>
+       <p>{{ introduction }}</p>
     </div>
 </template>
 
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            introduction:
+                "This is my layout training website that I'm going to use to practice copying webpages. I will be trying to use CSS grid most of the time, using the minimal amount of code possible to achieve the correct responsive designs."
+        };
+    }
+};
 </script>
 
 <style lang="scss">
@@ -17,11 +22,11 @@ export default {};
     display: grid;
     grid-template-columns: 1fr;
     align-items: center;
-    text-align: center;
     font-family: "Quicksand", "Source Sans Pro", -apple-system,
         BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial,
         sans-serif;
     h1 {
+        text-align: center;
         font-weight: 300;
         font-size: 100px;
         color: #35495e;
---
+++ pages/test.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <h1>{{ testText }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            testText: "This is a test"
+        };
+    }
+};
+</script>
+
+<style lang="scss">
+.container {
+    margin: 1rem;
+}
+</style>
